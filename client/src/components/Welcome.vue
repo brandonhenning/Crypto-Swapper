404661509b0b3f44b80f916a3751e8f08cc169a3
Brandon Henning <prescott.henning@gmail.com>
2018-07-30 13:19:53 -0600

    added welcome component in client

---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class='Welcome'>
+        <div class='logo-about'>
+            <div class='landing'>
+                <img src="../assets/swapt logo.png" alt="swapt logo" width="800px">
+                <p class='About'>Do you have what it takes to become the next crypto millionaire?</p>
+            </div>
+        </div>
+        <div class='play'>
+            <button class='play-button' type="submit">PLAY!</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Welcome'
+}   
+</script>
+
+
+<style>
+p {
+    color: #28ABE3;
+    font-size: 25px;
+}
+
+.play-button {
+    color: #C43E97;
+    font-size: 35px;
+    border-radius: 15px;
+}
+
+.play {
+    padding-top: 30px;
+}
+
+</style>
